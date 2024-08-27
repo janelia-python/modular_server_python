d77e2ce4ea0d32c05a5c0f53b93cfd1ce7765866
Peter Polidoro <peter@polidoro.io>
2024-08-27 16:13:37 -0400

    janelia-pypi -> janelia-python

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/janelia-pypi/modular_server_python',
+    url='https://github.com/janelia-python/modular_server_python',
 
     # Author details
     author='Peter Polidoro',
